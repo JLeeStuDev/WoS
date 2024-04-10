39dd418eb52f6a892c33aac48f33413046851989
JLeeStuDev <jlee.devspace@gmail.com>
2024-04-09 21:42:29 -0400

    Added background image for main menu

--- app/src/main/java/com/example/worldofstucraft/MainActivity.kt
+++ app/src/main/java/com/example/worldofstucraft/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.worldofstucraft
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,6 +16,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.worldofstucraft.ui.theme.WorldOfSTUcraftTheme
@@ -32,10 +38,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainMenuScreen() {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        // Background Image
+        Image(
+            painter = painterResource(id = R.drawable.mainmenu_bg),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+        )
+
         // Main Menu Layout
         Column(
             modifier = Modifier.fillMaxSize(),
